9bd34cfd133eed5ab3439a0872424191d526630c
kevlarr <echo1@dvnl.net>
2019-09-03 10:00:26 -0400

    Assign rendered cells to socket

--- lib/game_of_life_web/live/universe_live.ex
+++ lib/game_of_life_web/live/universe_live.ex
@@ -5,27 +5,30 @@ defmodule GameOfLifeWeb.UniverseLive do
 
   def render(assigns) do
     #<h2 phx-click="boom">It's <%= strftime!(@date, "%r") %></h2>
-    cells = Universe.render("u1")
 
     ~L"""
     <div>
       <h1>It's time for... the Game... of... LIFE!</h1>
-      <p style="width: 300px;"><%= cells %></p>
+      <p style="width: 300px;"><%= @cells %></p>
     </div>
     """
   end
 
   def mount(_session, socket) do
     if connected?(socket), do: :timer.send_interval(1000, self(), :tick)
-    universe = Universe.Supervisor.start_child("u1", {32, 32})
+    universe = Universe.Supervisor.start_child("u1", {8, 8})
 
-    {:ok, socket}
+    {:ok, put_cells(socket)}
   end
 
   def handle_info(:tick, socket) do
     # TODO get universe name from socket
     Universe.tick("u1")
 
-    {:noreply, socket}
+    {:noreply, put_cells(socket)}
+  end
+
+  defp put_cells(socket) do
+    assign(socket, cells: Universe.render("u1"))
   end
 end
