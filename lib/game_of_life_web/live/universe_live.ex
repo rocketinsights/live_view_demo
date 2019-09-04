dad0b52b5012f09477f10f9ab2daf1d683ab730d
kevlarr <echo1@dvnl.net>
2019-09-03 20:58:53 -0400

    Render grid

--- lib/game_of_life_web/live/universe_live.ex
+++ lib/game_of_life_web/live/universe_live.ex
@@ -1,34 +1,65 @@
 defmodule GameOfLifeWeb.UniverseLive do
   use Phoenix.LiveView
-  import Calendar.Strftime
+  use Phoenix.HTML, only: raw/1
   alias GameOfLife.Universe
 
   def render(assigns) do
-    #<h2 phx-click="boom">It's <%= strftime!(@date, "%r") %></h2>
-
     ~L"""
-    <div>
+    <div id="universe-page">
       <h1>It's time for... the Game... of... LIFE!</h1>
-      <p style="width: 300px;"><%= @cells %></p>
+
+      <div class="universe">
+        <%= for row <- @cells do %>
+          <div class="cell-row">
+            <%= for cell <- row do %>
+              <%= raw cell %>
+            <% end %>
+          </div>
+        <% end %>
+      </div>
     </div>
     """
   end
 
   def mount(_session, socket) do
-    if connected?(socket), do: :timer.send_interval(1000, self(), :tick)
-    universe = Universe.Supervisor.start_child("u1", {8, 8})
+    if connected?(socket), do: :timer.send_interval(500, self(), :tick)
+
+    socket = assign(socket,
+      universe: rand_bytes(),
+      dimensions: {8, 8},
+    )
+
+    Universe.Supervisor.start_child(
+      socket.assigns.universe,
+      socket.assigns.dimensions
+    )
 
     {:ok, put_cells(socket)}
   end
 
   def handle_info(:tick, socket) do
-    # TODO get universe name from socket
-    Universe.tick("u1")
+    Universe.tick(socket.assigns.universe)
 
     {:noreply, put_cells(socket)}
   end
 
+  defp rand_bytes, do: :crypto.strong_rand_bytes(16)
+
   defp put_cells(socket) do
-    assign(socket, cells: Universe.render("u1"))
+    cells = Universe.info(socket.assigns.universe)
+
+    assign(socket, cells: render_cells(cells))
+  end
+
+  def render_cells(cells) do
+    Enum.map(cells, fn row ->
+      Enum.map(row, fn %{alive: alive} ->
+        case alive do
+          nil -> "<div class='cell'>-</div>"
+          false -> "<div class='cell dead'></div>"
+          true -> "<div class='cell alive'></div>"
+        end
+      end)
+    end)
   end
 end
