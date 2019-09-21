b7db412e5c7040547e03d9c02c6a92f08c51d641
kevlarr <echo1@dvnl.net>
2019-09-21 10:51:06 -0400

    Add play/pause button

--- lib/game_of_life_web/live/universe_live.ex
+++ lib/game_of_life_web/live/universe_live.ex
@@ -10,13 +10,11 @@ defmodule GameOfLifeWeb.UniverseLive do
   end
 
   def mount(_session, socket) do
-    socket = assign(socket, universe: rand_bytes(), speed: 10)
+    socket = assign(socket, universe: rand_bytes(), speed: 10, playing: true)
 
     socket = assign(socket, template: :random, dimensions: %Dimensions{width: 16, height: 16})
     # socket = assign(socket, template: :beacon, dimensions: Template.dimensions(:beacon))
     # socket = assign(socket, template: :pulsar, dimensions: Template.dimensions(:pulsar))
-
-    if connected?(socket), do: schedule_tick(socket)
 
     Universe.start_link(%{
       name: socket.assigns.universe,
@@ -28,24 +26,38 @@ defmodule GameOfLifeWeb.UniverseLive do
   end
 
   def handle_info(:tick, socket) do 
-    socket =
-      socket
-      |> put_generation(&Universe.tick/1)
-      |> schedule_tick()
-
-    {:noreply, socket}
+    {:noreply, put_generation(socket, &Universe.tick/1)}
   end
 
   def handle_event("update_speed", %{"universe" => %{ "speed" => speed }}, socket) do
     {:noreply, assign(socket, speed: String.to_integer(speed))}
   end
 
-  defp put_generation(socket, f), do: assign(socket, generation: f.(socket.assigns.universe))
+  def handle_event("toggle_playing", _params, socket) do
+    {:noreply, toggle_playing(socket)}
+  end
+
+  defp put_generation(socket, f) do
+    socket
+    |> assign(generation: f.(socket.assigns.universe))
+    |> schedule_tick()
+  end
 
   defp rand_bytes, do: :crypto.strong_rand_bytes(16)
 
   defp schedule_tick(socket) do
-    Process.send_after(self(), :tick, trunc(1000 / socket.assigns.speed))
+    if connected?(socket) and socket.assigns.playing do
+      Process.send_after(self(), :tick, trunc(1000 / socket.assigns.speed))
+    end
+    socket
+  end
+
+  defp toggle_playing(socket) do
+    socket =
+      socket
+      |> assign(playing: !socket.assigns.playing)
+      |> schedule_tick()
+
     socket
   end
 end
