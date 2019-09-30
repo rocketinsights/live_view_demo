f7b15510eb74d926a2cce42f06cf24890cf1b451
Jonathan Principe <jprincipe@jonnys-mbp.lan>
2019-09-30 16:40:43 -0400

    update prod domain name

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :game_of_life, GameOfLifeWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "the.flameof.live", port: 80],
+  check_origin: ["//the.flameof.life"],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
