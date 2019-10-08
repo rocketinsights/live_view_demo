6bcd4519f9c86088719e8f3ca049e874d212cab7
Jon Principe <jonathan.principe@gmail.com>
2019-10-07 22:23:20 -0400

    try moving app to render

--- config/releases.exs
+++ config/releases.exs
@@ -1,6 +1,13 @@
 import Config
 
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
+
 config :game_of_life, GameOfLifeWeb.Endpoint,
-  server: true,
-  http: [port: {:system, "PORT"}],
-  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
+  http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
+  secret_key_base: secret_key_base,
+  server: true
